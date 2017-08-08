201f91bde6922e3c0b3b2bc84f4793ae522a0396
cmelthratter <cody.melthratter@sjsu.edu>
2017-08-08 15:04:39 -0700

    Changed Set attribute of Entry class to RPE. Much more useful metric. Input dialogs now sanitize input in case user forgets to put a value.

--- app/src/main/java/com/cmelthratter/strengthlog/ui/activities/EntryActivity.kt
+++ app/src/main/java/com/cmelthratter/strengthlog/ui/activities/EntryActivity.kt
@@ -10,27 +10,29 @@ import android.widget.*
 import com.cmelthratter.strengthlog.R
 import com.cmelthratter.strengthlog.ui.dialogs.EntryInputDialog
 import com.cmelthratter.strengthlog.ui.dialogs.RepsEditorDialog
+import com.cmelthratter.strengthlog.ui.dialogs.RpeEditorDialog
 import com.cmelthratter.strengthlog.ui.dialogs.WeightEditorDialog
 import com.cmelthratter.strengthlog.util.JsonHandler
 import com.cmelthratter.strengthlog.util.POSITION_KEY
-import java.text.SimpleDateFormat
-import java.util.*
 
+
+const val WEIGHT = 0
+const val RPE = 1
 /**
  * An activity for interacting with the list
  * of entries for a specified lift
  */
-class EntryActivity : AppCompatActivity() , EntryInputDialog.EntryDialogListener, RepsEditorDialog.RepsEditorDialogListener, WeightEditorDialog.WeightEditorDialogListener{
+class EntryActivity : AppCompatActivity() , EntryInputDialog.EntryDialogListener, RepsEditorDialog.RepsEditorDialogListener, RpeEditorDialog.EditorDialogListener{
 
     val TAG = EntryActivity::class.java.simpleName
 
-    lateinit var setsList : ListView
+    lateinit var rpeList: ListView
     lateinit var repsList : ListView
     lateinit var weightList : ListView
     lateinit var dateLabel : TextView
     lateinit var currentLift : Lift
     lateinit var currentEntry : Entry
-    lateinit var setsAdapter: ArrayAdapter<Int>
+    lateinit var rpeAdapter: ArrayAdapter<Float>
     lateinit var repsAdapter : ArrayAdapter<Int>
     lateinit var weightAdapter : ArrayAdapter<Float>
     lateinit var jsonHandler : JsonHandler
@@ -46,13 +48,17 @@ class EntryActivity : AppCompatActivity() , EntryInputDialog.EntryDialogListener
         currentEntry = currentLift.entries[entryPosition]
 
         val fab = findViewById(R.id.entry_fab) as FloatingActionButton
-        setsList = findViewById(R.id.sets_listView) as ListView
+        rpeList = findViewById(R.id.rpe_listView) as ListView
         repsList = findViewById(R.id.reps_listView) as ListView
         weightList = findViewById(R.id.weight_listView) as ListView
         dateLabel = findViewById(R.id.date_text_label) as TextView
         val toolbar = findViewById(R.id.entry_toolbar) as Toolbar
         toolbar.title = LiftActivity.currentLift.name
         setSupportActionBar(toolbar)
+        if(currentEntry.rpe.size != currentEntry.reps.size) {
+            currentEntry.rpe.clear()
+            currentEntry.rpe.addAll(currentEntry.weight)
+        }
 
 
         repsList.setOnItemClickListener { parent, view, position, id ->
@@ -68,11 +74,17 @@ class EntryActivity : AppCompatActivity() , EntryInputDialog.EntryDialogListener
             selectedPosition = position
         }
 
-        setsAdapter = ArrayAdapter<Int>(this, android.R.layout.simple_list_item_1, currentEntry.sets)
+        rpeList.setOnItemClickListener { parent, view, position, id ->
+            val dialog = RpeEditorDialog()
+            dialog.show(fragmentManager, "RpeEditorDialog")
+            selectedPosition = position
+        }
+
+        rpeAdapter = ArrayAdapter<Float>(this, android.R.layout.simple_list_item_1, currentEntry.rpe)
         repsAdapter = ArrayAdapter<Int>(this, android.R.layout.simple_list_item_1, currentEntry.reps)
         weightAdapter = ArrayAdapter<Float>(this, android.R.layout.simple_list_item_1, currentEntry.weight)
 
-        setsList.adapter = setsAdapter
+        rpeList.adapter = rpeAdapter
         repsList.adapter = repsAdapter
         weightList.adapter = weightAdapter
 
@@ -83,9 +95,11 @@ class EntryActivity : AppCompatActivity() , EntryInputDialog.EntryDialogListener
         fab.setOnClickListener { view ->
             var repsPlaceholder = 0
             var weightPlaceholder = 0.0F
+            var rpePlaceHolder = 0.0F
             if (currentEntry.reps.isNotEmpty()) repsPlaceholder = currentEntry.reps.last()
             if (currentEntry.weight.isNotEmpty()) weightPlaceholder = currentEntry.weight.last()
-            val dialog = EntryInputDialog(repsPlaceholder, weightPlaceholder)
+            if (currentEntry.rpe.isNotEmpty()) rpePlaceHolder = currentEntry.rpe.last()
+            val dialog = EntryInputDialog(repsPlaceholder, weightPlaceholder, rpePlaceHolder)
             dialog.show(fragmentManager, "EntryInputDialog")
 
         }
@@ -97,26 +111,45 @@ class EntryActivity : AppCompatActivity() , EntryInputDialog.EntryDialogListener
     }
 
 
-    override fun onDialogPositiveClick(reps: Int?, weight: Float?) {
-        setsAdapter.add(currentEntry.sets.size + 1)
+    override fun onDialogPositiveClick(reps: Int?, weight: Float?, rpe: Float?) {
 
-        if (reps == null) repsAdapter.add(0)
-        else repsAdapter.add(reps)
-        if (weight == null) weightAdapter.add(0.0F)
-        else weightAdapter.add(weight)
+        if (reps == null)
+            repsAdapter.add(0)
+        else
+            repsAdapter.add(reps)
+        if (weight == null)
+            weightAdapter.add(0.0F)
+        else
+            weightAdapter.add(weight)
+        if (rpe == null)
+            rpeAdapter.add(0.0F)
+        else
+            rpeAdapter.add(rpe)
+
         jsonHandler.writeLifts()
 
     }
-    override fun onDialogPositiveClick(newReps: Int) {
-        currentEntry.reps[selectedPosition] = newReps
+    override fun onDialogPositiveClick(newVal: Int) {
+
+        currentEntry.reps[selectedPosition] = newVal
         repsAdapter.notifyDataSetChanged()
         jsonHandler.writeLifts()
     }
 
-    override fun onDialogPositiveClick(newWeight: Float) {
-        currentEntry.weight[selectedPosition] = newWeight
-        weightAdapter.notifyDataSetChanged()
-        jsonHandler.writeLifts()
+
+    override fun onDialogPositiveClick(newVal: Float, type: Int) {
+        when(type) {
+            WEIGHT -> {
+             currentEntry.weight[selectedPosition] = newVal
+             weightAdapter.notifyDataSetChanged()
+             jsonHandler.writeLifts()
+            }
+            RPE -> {
+                currentEntry.rpe[selectedPosition] = newVal
+                weightAdapter.notifyDataSetChanged()
+                jsonHandler.writeLifts()
+            }
+        }
     }
 
     override fun onDialogNegativeClick() {
